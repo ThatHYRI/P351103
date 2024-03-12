30e5fc38f00a0951cb61c35a1317931ffd208c87
ThatHYRI <156666179+ThatHYRI@users.noreply.github.com>
2024-03-12 22:41:56 +0100

    1203STD3

--- code/src/devlog.c
+++ code/src/devlog.c
@@ -1,38 +1,8 @@
+#include "devlog.h"
 #include <stdio.h>
 
-int main(void)
-{
-    // define two ints
-    int x = 5;
-    int y = 4;
-
-    printf("x = %i, y = %i\n", x, y);
-
-    // define two pointers
-    int* p_x = &x;
-    int* p_y = &y;
-
-    // use the pointers and a temp variable to swap the values
-    int temp = *p_x;
-    *p_x = *p_y;
-    *p_y = temp;
-
-    printf("swapped: x = %i, y = %i\n", x, y);
-
-    //printf("                                                                      ");
-    //printf("          oooooooo                           oooooooo                 ");
-    //printf("      oo            oo                   oo            oo             ");
-    //printf("    ooooooooo oooooooooo               o                  o           ");
-    //printf("  o      ooo  ooo        o      ooooooooooooooooooooooooo   o         ");
-    //printf(" o      ooo   oooooo      o         o         ooooooooo      o        ");
-    //printf(" o     ooo    ooo         o         o      ooooooooo         o        ");
-    //printf("  o   ooo     ooo        o           o   ooooooooooooooooooooooooo    ");
-    //printf("    oooooooooooooooooooo               o                  o           ");
-    //printf("      oo            oo                   oo            oo             ");
-    //printf("          oooooooo                           oooooooo                 ");
-    //printf("                                   O        P        E        L       ");
-
-    //test print
+//ZF OPEL Logo
+void print_devlog() {
     printf("                                                                      ");
     printf("                                                                      ");
     printf("          oooooooo                           oooooooo                 ");
@@ -59,4 +29,11 @@ int main(void)
     printf("                                                                      ");
     printf("                                                                      ");
     printf("                                                                      ");
+}
+
+
+//placeholder main
+int main() {
+    print_devlog();
+    return 35;
 }
---
+++ code/src/zofelmain.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//include other headers
+//#include 
+//#include 
+//#include 
+
+int main() {
+
+    //placeholder
+    //placeholder
+
+
+    while(1) {
+        printf("==================================");
+        printf("      [ CHOOSE YOUR ACTION ]      ");
+        printf("       List Developers  [1]       ");
+        printf("       Print Group Logo [2]       ");
+        printf("       Print Group      [3]       ");
+        printf("       EXIT             [4]       ");
+        printf("==================================");
+    }
+
+}
